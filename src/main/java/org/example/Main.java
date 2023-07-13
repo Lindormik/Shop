e48dd4303dfb595f284f2b0b4ff74fad81d2d91d
Wojciech Mojecki <mojeckiwojtek@gmail.com>
2023-07-13 22:04:26 +0200

    commit and push check 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Commit and push check");
+        System.out.println("Commit and push check");
+        System.out.println("Commit and push check");
+        System.out.println("Commit and push check");
 
     }
 }
