bb4dc9f40874b2c48b0073c4d84937f338220184
Kamil Turek <kamilturek2424@gmail.com>
2023-07-17 18:51:49 +0200

    model Product

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,13 +1,89 @@
 package org.example;
 
+import static java.util.prefs.Preferences.MAX_NAME_LENGTH;
+
 public class Product {
 
     private int productID;
     private double price;
-    private String surname;
+    private String name;
+    private int quantity;
+    private Category category;
+    public Product(int productID, double price, String name, Category category, int quantity) {
+        this.productID = productID;
+        this.price = validatePrice(price);
+        this.name = validateName(name);
+        this.category = validateCategory(category);
+        this.quantity = validateQuantity(quantity);
+    }
 
+    private double validatePrice(double price) {
+        if (price <= 0) {
+            throw new IllegalArgumentException("Cena musi być większa od 0");
+        }
+        return price;
+    }
 
+    private String validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Pole Imię nie może być puste");
+        }
+        if (name.length() <= 0 || name.length() >= 50) {
+            throw new IllegalArgumentException("Pole Imię musi zawierać od 1 do 50 znaków");
+        }
+        if (!name.matches("[a-zA-Z]+")) {
+            throw new IllegalArgumentException("Pole Imię musi zawierać znaki z alfabetu");
+        }
+        return name;
+    }
 
+    private Category validateCategory(Category category) {
+        if (category == null) {
+            throw new IllegalArgumentException("Pole Kategoria nie może być puste");
+        }
+        return category;
+    }
 
+    private int validateQuantity(int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Ilość musi być większa od 0");
+        }
+        return quantity;
+    }
 
+    public int getProductID() {
+        return productID;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = validatePrice(price);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = validateName(name);
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = validateCategory(category);
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = validateQuantity(quantity);
+    }
 }
