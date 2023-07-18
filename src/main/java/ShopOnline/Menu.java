c6d0a421d7ee3101b72e84fadf2140d1f3df214d
Wojciech Mojecki <mojeckiwojtek@gmail.com>
2023-07-18 19:29:06 +0200

    classess updated: Category, CategoryService, Product, ProductService

--- src/main/java/ShopOnline/Menu.java
+++ src/main/java/ShopOnline/Menu.java
@@ -4,7 +4,6 @@ import ShopOnline.services.CategoryService;
 import ShopOnline.services.ProductService;
 
 import java.util.Scanner;
-
 public class Menu extends ProductService {
     private final Scanner scanner = new Scanner(System.in);
     private final ProductService productService = new ProductService();
@@ -23,7 +22,7 @@ public class Menu extends ProductService {
 
             switch (choice) {
                 case 1 -> showProductSubMenu();
-//                case 2 -> showCategorySubMenu();
+                case 2 -> showCategorySubMenu();
 //                case 2 -> showOrderSubMenu();
                 case 3 -> exit = true;
                 default -> System.out.println("Nieprawidłowy wybór. Spróbuj ponownie.");
@@ -38,16 +37,17 @@ public class Menu extends ProductService {
         while (!back) {
             System.out.println("[1] Pokaż wszystkie produkty.");
             System.out.println("[2] Pokaż jeden produkt.");
-            System.out.println("[3] Dodaj produkt.");
+            System.out.println("[3,Nazwa produktu, Cena produktu, Ilosc produktu] Dodaj produkt.");
             System.out.println("[4] Cofnij");
 
-            int choice = scanner.nextInt();
+            String choice = scanner.next();
+            String[] words = choice.split(",");
             scanner.nextLine();
 
-            switch (choice) {
+            switch (Integer.parseInt(words[0])) {
                 case 1 -> productService.showAllProducts();
                 case 2 -> productService.showOneProduct();
-                case 3 -> productService.addProduct();
+                case 3 -> productService.addProduct(words[1], Double.parseDouble(words[2]),Integer.parseInt(words[3]));
                 case 4 -> back = true;
                 default -> System.out.println("Nieprawidłowy wybór. Spróbuj ponownie.");
             }
@@ -55,4 +55,25 @@ public class Menu extends ProductService {
             System.out.println();
         }
     }
+    public void showCategorySubMenu() {
+        boolean back = false;
+        while (!back) {
+            System.out.println("[1] Pokaż wszystkie kategorie.");
+            System.out.println("[2,IdKategorii] Pokaż wybrana kategorie.");
+            System.out.println("[3,Nazwa kategorii] Dodaj kategorie.");
+            System.out.println("[4] Cofnij");
+            String choice = scanner.next();
+            String[] words = choice.split(",");
+            scanner.nextLine();
+            switch (Integer.parseInt(words[0])) {
+                case 1 -> categoryServiceService.showAllCategories();
+                case 2 -> categoryServiceService.findCategoryById(Integer.parseInt(words[1]));
+                case 3 -> categoryServiceService.addCategory(words[1]);
+                case 4 -> back = true;
+                default -> System.out.println("Nieprawidłowy wybór. Spróbuj ponownie.");
+            }
+            System.out.println();
+        }
+    }
+}
 }
